9d2d5c495bc03c5a436e5281739916310046353c
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-07-20 22:18:57 -0400

    Add server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return "Hello World";
+});
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.info("HTTP server runnig on http://localhost:3333");
+}).catch(() => {
+  console.info("Error Server");
+}) 
