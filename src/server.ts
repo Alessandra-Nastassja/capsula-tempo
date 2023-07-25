ebec48979c791e0bd535808c74ce89b5f6c0d46c
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-07-25 18:43:39 -0400

    Setting configs the better code

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/hello', () => {
-  return "Hello World";
-});
+  return 'Hello World'
+})
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.info("HTTP server runnig on http://localhost:3333");
-}).catch(() => {
-  console.info("Error Server");
-}) 
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.info('HTTP server runnig on http://localhost:3333')
+  })
+  .catch(() => {
+    console.info('Error Server')
+  })
